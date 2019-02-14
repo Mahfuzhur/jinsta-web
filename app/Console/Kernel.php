a396730afb36ffaebfcac52a20bef6b7de3275df
Mahfuzhur <Mahfuzhur@users.noreply.github.com>
2019-02-14 18:59:25 +0600

    template deleted

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,46 +30,47 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        try{
-
-        }catch(\Exception $ex){
-            echo $ex;
-        }
+        $this->user = DB::table('users')
+            ->join('user_schedule', 'users.id', '=','user_schedule.user_id' )
+            ->join('schedule', 'schedule.id', '=', 'user_schedule.schedule_id')
+            ->join('hashtag_schedule', 'hashtag_schedule.schedule_id', '=', 'schedule.id')
+            ->join('template_schedule', 'template_schedule.schedule_id', '=', 'schedule.id')
+            ->join('template', 'template.id', '=', 'template_schedule.template_id')
+            ->join('hashtag', 'hashtag.id', '=', 'hashtag_schedule.hashtag_id')
+            ->join('client', 'client.hashtag_id', '=', 'hashtag.id')
+            ->select('users.name','users.instagram_username','users.instagram_password','schedule.delivery_period_start','schedule.delivery_period_end'
+                ,'schedule.date_exclusion_setting_start','schedule.date_exclusion_setting_end'
+                ,'schedule.specify_time_start','schedule.specify_time_end', 'schedule.time_exclusion_setting_start'
+                , 'schedule.time_exclusion_setting_end','hashtag.hashtag','client.user_id','client.client_id',
+                'client.hashtag_id','template.title','template.description','template.image')
+            ->where([['client.dm_sent','!=','1']])
+            ->groupBy('hashtag.hashtag')
+            ->first();
         $schedule->call(function () {
             try{
-                $user = DB::table('users')
-                    ->join('user_schedule', 'users.id', '=','user_schedule.user_id' )
-                    ->join('schedule', 'schedule.id', '=', 'user_schedule.schedule_id')
-                    ->join('hashtag_schedule', 'hashtag_schedule.schedule_id', '=', 'schedule.id')
-                    ->join('template_schedule', 'template_schedule.schedule_id', '=', 'schedule.id')
-                    ->join('template', 'template.id', '=', 'template_schedule.template_id')
-                    ->join('hashtag', 'hashtag.id', '=', 'hashtag_schedule.hashtag_id')
-                    ->join('client', 'client.hashtag_id', '=', 'hashtag.id')
-                    ->select('users.name','users.instagram_username','users.instagram_password','schedule.delivery_period_start','schedule.delivery_period_end'
-                        ,'schedule.date_exclusion_setting_start','schedule.date_exclusion_setting_end'
-                        ,'schedule.specify_time_start','schedule.specify_time_end', 'schedule.time_exclusion_setting_start'
-                        , 'schedule.time_exclusion_setting_end','hashtag.hashtag','client.user_id','client.client_id',
-                        'client.hashtag_id','template.title','template.description','template.image')
-                    ->where([['client.dm_sent','!=','1']])
-                    ->groupBy('hashtag.hashtag')
-                    ->first();
+
+
                 $this->ig = new \InstagramAPI\Instagram();
 
-                $result = $this->ig->login($user->instagram_username,$user->instagram_password);
+                $result = $this->ig->login($this->user->instagram_username,$this->user->instagram_password);
                 $recipents = [
-                    'users' => [$user->client_id]
+                    'users' => [$this->user->client_id]
                 ];
 
-                $imagePath = 'uploads/'.$user->image;
-                $this->ig->direct->sendText($recipents,$user->title);
-                $this->ig->direct->sendPhoto($recipents,$imagePath);
-                $this->ig->direct->sendText($recipents,$user->description);
+                $imagePath = 'uploads/'.$this->user->image;
+                $this->ig->direct->sendText($recipents,$this->user->title);
+//                $this->ig->direct->sendPhoto($recipents,$imagePath);
+//                $this->ig->direct->sendText($recipents,$this->user->description);
 
             }catch (\Exception $ex){
                 echo $ex;
             }
-        })->between($user->delivery_period_start,$user->delivery_period_end)->unlessBetween($user->date_exclusion_setting_start,$user->date_exclusion_setting_end);
-        $schedule->command('log:demo')->everyMinute();
+        })->between($this->user->delivery_period_start,$this->user->delivery_period_end)
+            ->unlessBetween($this->user->date_exclusion_setting_start,$this->user->date_exclusion_setting_end)
+            ->between($this->user->specify_time_start,$this->user->specify_time_end)
+            ->unlessBetween($this->user->time_exclusion_setting_start,$this->user->time_exclusion_setting_end)
+            ->everyMinute();
+        //$schedule->command('log:demo')->everyMinute();
 //        $schedule->call(function () {
 //            \Log::info('i was here');
 //       })->everyMinute();
